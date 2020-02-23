2d56bf07d9366a941d8fab37221a0fbb7fb0dceb
sunbugbug <ygy0803@gmail.com>
2020-02-24 04:53:26 +0900

    FILE을 사용한 Graph 생성 및 BFS를 위한 Queue생성

---
+++ RRS/RRS/Graph.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRS
+{
+    class Graph
+    {
+        private static Graph _gInstance;
+
+        private Dictionary<string, Vertex> _dicVertex = new Dictionary<string, Vertex>();
+
+        public static Graph GetInstance()
+        {
+            if (_gInstance == null)
+            {
+                _gInstance = new Graph();
+            }
+
+            return _gInstance;
+        }
+
+        public void printGraph()
+        {
+            foreach (var key in _dicVertex.Keys)
+            {
+                Vertex v = _dicVertex[key];
+                
+                foreach (Edge e in v.getAdjList())
+                {
+                    Console.Write("{0} ", key);
+                    e.printEdge();
+                }
+            }
+        }
+
+        public void addVertex(Vertex v, string key)
+        {
+            _dicVertex.Add(key, v);
+        }
+
+        public void addEdges(string from, string to, int capa)
+        {
+            _dicVertex.TryGetValue(from, out Vertex v);
+            v.getAdjList().Add(new Edge(to, capa));
+        }
+
+        public Vertex getVertex(string key)
+        {
+            return _dicVertex[key];
+        }
+    }
+
+    class Vertex
+    {
+        private List<Edge> _adjList = new List<Edge>();
+
+        public List<Edge> getAdjList()
+        {
+            return _adjList;
+        }
+
+        public List<Edge> getEdges()
+        {
+            return _adjList;
+        }
+    }
+
+    class Edge
+    {
+        private string to;
+        private int capacity;
+
+        public Edge(string to, int capa)
+        {
+            this.to = to;
+            this.capacity = capa;
+        }
+
+        public void printEdge()
+        {
+            Console.WriteLine("{0} {1}", to, capacity);
+        }
+
+        public int getCapa()
+        {
+            return capacity;
+        }
+
+        public string getTo()
+        {
+            return to;
+        }
+    }
+}
---
+++ RRS/RRS/main.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRS
+{
+    class main
+    {
+        public static Graph g = Graph.GetInstance();
+
+        public static Queue<Vertex> q = new Queue<Vertex>();
+
+        static void Main(string[] args)
+        {
+            #region FILE로 Graph구성
+            FileStream fs = new FileStream("input.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            string edge;
+
+            string[] vertices = sr.ReadLine().Split(' ');
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                Vertex v = new Vertex();
+                g.addVertex(v, i.ToString());
+            }
+
+            while((edge = sr.ReadLine()) != null){
+                string from, to;
+                int capa;
+
+                from = edge.Split(' ')[0].ToString();
+                to = edge.Split(' ')[1].ToString();
+                capa = Convert.ToInt32(edge.Split(' ')[2]);
+
+                g.addEdges(from, to, capa);
+            }
+
+            g.printGraph();
+            #endregion FILE로 Graph구성
+            // DB 호출 -> _dicGraphInfo -> List<Edge>
+
+            #region BFS로 Routing
+            Vertex start = g.getVertex("0");
+
+            q.Enqueue(start);
+
+
+            #endregion
+        }
+    }
+}
